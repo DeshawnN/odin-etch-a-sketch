ed5062540269ece531a1783b890af04ea368fd99
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2021-12-03 18:18:31 -0500

    feat: add the ability to reset the grid based on user input

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-makeGrid(16, 16);
-addHoverEvents();
-function makeGrid(cols, rows) {
+initializer();
+
+function initializer() {
+    const resetButton = document.querySelector(".reset-button");
+
+    resetButton.addEventListener('click', resetGrid);
+
+    makeGrid(16, 16);
+}
+
+function makeGrid(width, height) {
     const container = document.querySelector('.grid-container');
-    container.style.setProperty('--rows', rows);
+    container.innerHTML = '';
+    container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${height}, 1fr)`;
     
-    for (let col = 0; col < cols; col++) {
-        for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < width; col++) {
+        for (let row = 0; row < height; row++) {
             const gridItem = document.createElement('div');
             gridItem.classList.add("grid-item");
             container.appendChild(gridItem);
         }
     }
+
+    addHoverEvents();
 }
 
 function addHoverEvents() {
@@ -22,7 +34,20 @@ function addHoverEvents() {
         });
 
         item.addEventListener('mouseleave', event => {
-            event.target.style.backgroundColor = "initial";
+            event.target.style.backgroundColor = item.style.getPropertyValue('--intial-color');
         })
     });
+}
+
+function resetGrid() {
+    const dimensions = prompt("Grid Dimensions: (format: WxH, so 16x16 for a 16 by 16 grid)", '16x16');
+    const columns = dimensions.split("x")[0];
+    const rows = dimensions.split("x")[1];
+
+    if (columns > 100 || rows > 100) {
+        alert("Please try again with a number smaller than 100");
+        return;
+    }
+
+    makeGrid(rows, columns);
 }
