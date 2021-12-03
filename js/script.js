69da397f6230657e789292ac4997f2874d2dc0fd
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2021-12-03 15:40:21 -0500

    refactor: move hover logic from css to javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 makeGrid(16, 16);
+addHoverEvents();
 function makeGrid(cols, rows) {
     const container = document.querySelector('.grid-container');
     container.style.setProperty('--rows', rows);
@@ -10,4 +11,18 @@ function makeGrid(cols, rows) {
             container.appendChild(gridItem);
         }
     }
+}
+
+function addHoverEvents() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    
+    gridItems.forEach(item => {
+        item.addEventListener('mouseover', event => {
+            event.target.style.backgroundColor = "lightblue";
+        });
+
+        item.addEventListener('mouseleave', event => {
+            event.target.style.backgroundColor = "initial";
+        })
+    });
 }
